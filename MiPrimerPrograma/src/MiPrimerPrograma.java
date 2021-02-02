51635b90b1f803191ad85bb59ad9f4514f15857e
januxdesa <januxdesa@users.noreply.github.com>
2021-02-02 13:40:20 -0300

    JAVA FUNDAMENTALS 1.- Primer Programa

---
+++ MiPrimerPrograma/src/MiPrimerPrograma.java
@@ -0,0 +1,10 @@
+
+public class MiPrimerPrograma {
+	
+	public static void main(String[] args) {
+        System.out.println("Mi nombre es Alejandro.");
+        System.out.println("Tengo 40 y tantos años de edad.");
+        System.out.println("Mi ciudad es Santiago.");
+    }
+	
+}
